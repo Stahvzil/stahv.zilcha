ee982584cf6c82e7939e8c04b88f474f9c53e788
Noam Shalev <ns898@cornell.edu>
2021-08-27 12:55:43 -0700

    adding server

---
+++ MyFirstServer.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end("Hellow World");
+}).listen(8080);
+
+var http = require('http');
+
+
